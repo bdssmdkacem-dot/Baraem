4faa34f2cd3c7e27ddd2cc16cd9a280d30e92aeb
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-08-27 00:12:28 +0100

    refactor: use dedicated star counter in home header

--- lib/widgets/home_header.dart
+++ lib/widgets/home_header.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../core/app_scale.dart';
-import '../theme/app_colors.dart';
+import 'star_counter.dart';
 
 class HomeHeader extends StatelessWidget {
   const HomeHeader({super.key, required this.stars});
@@ -26,32 +26,14 @@ class HomeHeader extends StatelessWidget {
                 'ماذا سنتعلم اليوم؟',
                 style: TextStyle(
                   fontSize: 14 * scale,
-                  color: AppColors.textMuted,
+                  color: Colors.grey,
                   fontWeight: FontWeight.w600,
                 ),
               ),
             ],
           ),
         ),
-        Semantics(
-          label: '$stars نجمة',
-          container: true,
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            decoration: BoxDecoration(
-              color: AppColors.surface,
-              borderRadius: BorderRadius.circular(18),
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Icon(Icons.star_rounded, color: AppColors.primaryGold),
-                const SizedBox(width: 4),
-                Text('$stars', style: const TextStyle(fontWeight: FontWeight.w800)),
-              ],
-            ),
-          ),
-        ),
+        StarCounter(stars: stars),
       ],
     );
   }
