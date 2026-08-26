0c311afe7ddad02b6f1ced9ef45f6bd262f47a45
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-08-26 22:23:17 +0100

    fix: import material types in responsive foundation test

--- test/foundation_smoke_test.dart
+++ test/foundation_smoke_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:baraem/core/app_scale.dart';
@@ -5,6 +6,8 @@ import 'package:baraem/core/app_scale.dart';
 void main() {
   testWidgets('responsive foundation stays usable on a compact screen', (tester) async {
     await tester.binding.setSurfaceSize(const Size(320, 640));
+
+    await tester.pumpWidget(const MaterialApp(home: _TestHost()));
 
     final context = tester.element(find.byType(_TestHost));
     expect(AppScale.horizontalPadding(context), 20);
