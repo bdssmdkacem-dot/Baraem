89b29e9a6b304bb22a4356b9560a50ddb0e83336
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-08-26 22:17:08 +0100

    test: add responsive foundation smoke test

---
+++ test/foundation_smoke_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:baraem/core/app_scale.dart';
+
+void main() {
+  testWidgets('responsive foundation stays usable on a compact screen', (tester) async {
+    await tester.binding.setSurfaceSize(const Size(320, 640));
+
+    final context = tester.element(find.byType(_TestHost));
+    expect(AppScale.horizontalPadding(context), 20);
+    expect(AppScale.fontScale(context), 0.92);
+
+    await tester.binding.setSurfaceSize(null);
+  });
+}
+
+class _TestHost extends StatelessWidget {
+  const _TestHost();
+
+  @override
+  Widget build(BuildContext context) => const SizedBox.shrink();
+}
