2cb21e69ad02e6acb34a42fdec3672d46c1a4236
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-08-26 22:16:57 +0100

    feat: install global error handling at app startup

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'app.dart';
+import 'core/app_error_handler.dart';
 import 'providers/app_state_provider.dart';
 import 'providers/audio_provider.dart';
 import 'providers/progress_provider.dart';
@@ -11,26 +12,29 @@ import 'providers/purchase_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  AppErrorHandler.install();
 
-  final appStateProvider = AppStateProvider();
-  await appStateProvider.load();
+  runGuardedApp(() async {
+    final appStateProvider = AppStateProvider();
+    await appStateProvider.load();
 
-  final progressProvider = ProgressProvider();
-  await progressProvider.load();
+    final progressProvider = ProgressProvider();
+    await progressProvider.load();
 
-  final purchaseProvider = PurchaseProvider()
-    ..onPremiumChanged = (isPremium) => progressProvider.setPremium(isPremium);
-  unawaited(purchaseProvider.init());
+    final purchaseProvider = PurchaseProvider()
+      ..onPremiumChanged = (isPremium) => progressProvider.setPremium(isPremium);
+    unawaited(purchaseProvider.init());
 
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(value: appStateProvider),
-        ChangeNotifierProvider.value(value: progressProvider),
-        ChangeNotifierProvider.value(value: purchaseProvider),
-        ChangeNotifierProvider(create: (_) => AudioProvider()),
-      ],
-      child: const BaraemApp(),
-    ),
-  );
+    runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(value: appStateProvider),
+          ChangeNotifierProvider.value(value: progressProvider),
+          ChangeNotifierProvider.value(value: purchaseProvider),
+          ChangeNotifierProvider(create: (_) => AudioProvider()),
+        ],
+        child: const BaraemApp(),
+      ),
+    );
+  });
 }
