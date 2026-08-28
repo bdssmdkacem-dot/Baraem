1ccb55e451f5a9b2b53f11f2a1715dcc80dfd2c4
ressourcesapmm-lgtm <ressourcesapmm@gmail.com>
2026-08-28 03:50:50 +0100

    test: provide audio provider for challenge screen

--- test/friends_challenge_ui_test.dart
+++ test/friends_challenge_ui_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:baraem/models/player_profile.dart';
+import 'package:baraem/providers/audio_provider.dart';
 import 'package:baraem/providers/players_provider.dart';
 import 'package:baraem/screens/friends_challenge_screen.dart';
 
@@ -12,8 +13,21 @@ void main() {
       'baraem_players_v1': '[{"id":"1","nickname":"آدم","age":6,"gender":"boy","stars":2},{"id":"2","nickname":"سارة","age":11,"gender":"girl","stars":5}]',
     });
     final players = PlayersProvider();
+    final audio = AudioProvider();
     await players.load();
-    await tester.pumpWidget(ChangeNotifierProvider.value(value: players, child: const MaterialApp(home: FriendsChallengeScreen())));
+    addTearDown(() {
+      audio.dispose();
+      players.dispose();
+    });
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(value: players),
+          ChangeNotifierProvider.value(value: audio),
+        ],
+        child: const MaterialApp(home: FriendsChallengeScreen()),
+      ),
+    );
     await tester.pumpAndSettle();
     expect(find.text('آدم'), findsOneWidget);
     expect(find.text('سارة'), findsOneWidget);
@@ -33,5 +47,6 @@ void main() {
     expect(players.getById('1')!.stars, 1);
     expect(players.getById('1')!.questionsAnswered, 1);
     expect(players.getById('1')!.correctAnswers, 1);
+    players.dispose();
   });
 }
