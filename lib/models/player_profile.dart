d4b82d4b51e2fa71e916c47b726d191fa1ad5bce
ressourcesapmm-lgtm <ressourcesapmm@gmail.com>
2026-08-28 02:47:25 +0100

    feat: add local multiplayer player profiles

---
+++ lib/models/player_profile.dart
@@ -0,0 +1,13 @@
+class PlayerProfile {
+  const PlayerProfile({
+    required this.id,
+    required this.nickname,
+    required this.age,
+    required this.gender,
+  });
+
+  final String id;
+  final String nickname;
+  final int age;
+  final String gender;
+}
