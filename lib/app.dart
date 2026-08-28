bec721f4e48ed507c4f6f1fd4c4febc2190ec318
ressourcesapmm-lgtm <ressourcesapmm@gmail.com>
2026-08-28 02:22:39 +0100

    feat: show child profile setup on first launch

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 
 import 'core/app_routes.dart';
+import 'providers/progress_provider.dart';
 import 'screens/adab_screen.dart';
 import 'screens/adkar_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/profile_setup_screen.dart';
 import 'screens/stories_screen.dart';
 import 'theme/app_theme.dart';
 
@@ -13,9 +16,8 @@ class BaraemApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    debugPrint('[STARTUP] BaraemApp build');
-
-    final app = MaterialApp(
+    final profile = context.watch<ProgressProvider>();
+    return MaterialApp(
       title: 'براعم',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
@@ -32,17 +34,13 @@ class BaraemApp extends StatelessWidget {
         AppRoutes.adhkar: (_) => const AdkarScreen(),
         AppRoutes.manners: (_) => const AdabScreen(),
         AppRoutes.stories: (_) => const StoriesScreen(),
+        '/profile-setup': (_) => const ProfileSetupScreen(),
       },
-      initialRoute: AppRoutes.home,
-      builder: (context, child) {
-        return Directionality(
-          textDirection: TextDirection.rtl,
-          child: child ?? const SizedBox.shrink(),
-        );
-      },
+      initialRoute: profile.profileCompleted ? AppRoutes.home : '/profile-setup',
+      builder: (context, child) => Directionality(
+        textDirection: TextDirection.rtl,
+        child: child ?? const SizedBox.shrink(),
+      ),
     );
-
-    debugPrint('[STARTUP] MaterialApp created');
-    return app;
   }
 }
