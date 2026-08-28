5a5d65f6c4036af4e3d62cc2625ad00ab6073d89
ressourcesapmm-lgtm <ressourcesapmm@gmail.com>
2026-08-28 02:50:48 +0100

    test: cover friends challenge flow and persistent progress

---
+++ test/friends_challenge_ui_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../lib/models/player_profile.dart';
+import '../lib/providers/players_provider.dart';
+import '../lib/screens/friends_challenge_screen.dart';
+
+void main() {
+  testWidgets('friends challenge starts with two players and shows adaptive question', (tester) async {
+    SharedPreferences.setMockInitialValues({
+      'baraem_players_v1': '[{"id":"1","nickname":"آدم","age":6,"gender":"boy","stars":2},{"id":"2","nickname":"سارة","age":11,"gender":"girl","stars":5}]',
+    });
+    final players = PlayersProvider();
+    await players.load();
+    await tester.pumpWidget(ChangeNotifierProvider.value(value: players, child: const MaterialApp(home: FriendsChallengeScreen())));
+    await tester.pumpAndSettle();
+    expect(find.text('آدم'), findsOneWidget);
+    expect(find.text('سارة'), findsOneWidget);
+    await tester.tap(find.text('ابدأ التحدّي'));
+    await tester.pumpAndSettle();
+    expect(find.textContaining('دور'), findsOneWidget);
+    expect(find.text('السؤال التالي'), findsNothing);
+    expect(find.byType(FilledButton), findsWidgets);
+  });
+
+  test('player question result persists a star and progress', () async {
+    SharedPreferences.setMockInitialValues({});
+    final players = PlayersProvider();
+    await players.load();
+    await players.add(const PlayerProfile(id: '1', nickname: 'آدم', age: 6, gender: 'boy'));
+    await players.recordQuestionResult(playerId: '1', correct: true);
+    expect(players.getById('1')!.stars, 1);
+    expect(players.getById('1')!.questionsAnswered, 1);
+    expect(players.getById('1')!.correctAnswers, 1);
+  });
+}
